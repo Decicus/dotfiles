85e765b0b8cf1de19da22d67dcaedbe8c5088de6
Alex Thomassen <alex@thomassen.xyz>
2020-12-23 14:35:55 +0100

    [zsh] Tweak bootstrap script

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -3,11 +3,11 @@
 # Get directory of bootstrap script + .zshrc.
 ZSH_CONFDIR=$(dirname "$0")
 
-sudo apt install zsh git
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Run oh-my-zsh installer
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
 
 # Save the current .zshrc file (on host) to a backup file.
-mv $HOME/.zshrc $HOME/.zshrc.backup
+mv "${HOME}/.zshrc" "${HOME}/.zshrc.backup";
 
 # Copy the ZSH config to the home directory.
-cp $ZSH_CONFDIR/.zshrc $HOME/.zshrc
+cp "${ZSH_CONFDIR}/.zshrc" "${HOME}/.zshrc";
