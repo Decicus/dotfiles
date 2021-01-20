16c6ae6e3813afe14606f8147c45bb3977543cc2
Alex Thomassen <alex@thomassen.xyz>
2021-01-20 12:53:08 +0100

    [zsh] Change bootstrap script to the more 'manual' approach

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+# This script already assumes `zsh` and `git` are both already installed
+# via the initial setup (I always do this), so really all that happens here
+# Is the cloning and installation of `oh-my-zsh` and `.zshrc`.
+
 # Get directory of bootstrap script + .zshrc.
 ZSH_CONFDIR=$(dirname "$0")
 
 # Run oh-my-zsh installer
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh;
 
 # Save the current .zshrc file (on host) to a backup file.
 mv "${HOME}/.zshrc" "${HOME}/.zshrc.backup";
 
 # Copy the ZSH config to the home directory.
 cp "${ZSH_CONFDIR}/.zshrc" "${HOME}/.zshrc";
+
+# Prompt the user to change the shell.
+chsh -s $(which zsh);
